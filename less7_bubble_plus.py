8144f4f05be8e70744ab7ca9324c97f9e4d78aac
Daniil Dyachenko <daniilstv@gmail.com>
2019-05-12 23:22:03 +0300

    HW for Lesson 7
    К пузырьковой сортировке добавил решение задания 7.3 - нахождение медианы.
    Сначала сделал на отсортированном массиве, затем придумал собственный алгорити (без сортировки).

--- less7_bubble_plus.py
+++ less7_bubble_plus.py
@@ -2,9 +2,11 @@
 #%%
 import random
 import copy
-n = 13
-def rnd_lst(n):
-    rnd_list = [random.randint(-100, 100) for i in range(n)]
+n = 15
+min_rnd = -100
+max_rnd = 100 
+def rnd_lst(n, min_rnd = min_rnd , max_rnd = max_rnd):
+    rnd_list = [random.randint(min_rnd, max_rnd) for i in range(n)]
     print("Создали список из %d элементов:" % (n) )
     return rnd_list
 
@@ -37,7 +39,7 @@ print(*sort_to_max(a))
 Задачу можно решить без сортировки исходного массива. Но если это слишком сложно,
 то используйте метод сортировки, который не рассматривался на уроках.
 '''
-print("Решение задачи 7.3 по отсортированному массиву:")
+print("Решение задачи 7.3 по отсортированному массиву..")
 print(*a)
 if len(a)%2 == 0:
     print(a[len(a)//2], a[len(a)//2-1], "-> медиана:", ( int (a[len(a)//2]) + int(a[len(a)//2-1]) ) / 2 )
@@ -47,6 +49,31 @@ else:
 
 
 #%%
-# в работе
-# print("Решение задачи 7.3 по неотсортированному массиву:")
+print("Решение задачи 7.3 собственным алгоритмом по неотсортированному массиву с нечетным числом элементов..")
 b = copy.deepcopy(rnd_lst)
+print(*b)
+
+def id_min(lst, max_rnd = max_rnd):
+    x_min = max_rnd+1
+    x_min_idx = 0
+    for i in range(len(lst)):
+        if x_min > lst [i]:
+            x_min = lst [i]
+            x_min_idx = i
+    return x_min_idx
+
+def id_max(lst, min_rnd = min_rnd):
+    x_max = min_rnd - 1
+    x_max_idx = 0
+    for i in range(len(lst)):       
+        if x_max < lst [i] :
+            x_max = lst [i]
+            x_max_idx = i
+    return x_max_idx
+#%%
+for i in range(len(b)//2):
+ #   print("По индексу удаляю максимальное и минимальное значение в массиве. Индексы: ", id_max(b), id_min(b))
+    b.pop(id_max(b))
+    b.pop(id_min(b))
+
+print("Медиана:", *b)
